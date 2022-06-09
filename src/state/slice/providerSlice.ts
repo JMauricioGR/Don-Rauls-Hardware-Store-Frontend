725b541fc4a99dcfbf24549fa12dacbf1e67d2dd
Jehison Galarza <jehisonsc045@gmail.com>
2022-06-09 01:11:50 -0500

    Don Raul's Hardware Store --- Project structure, store and providerSlice created

---
+++ src/state/slice/providerSlice.ts
@@ -0,0 +1,37 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+type providerType = {
+  id: string,
+  name: string,
+  providerId: number,
+  note: string
+}
+
+const initialState: providerType[] = [
+  {
+    id: "1jh23kh32jh3",
+    name: "Provider 1",
+    providerId: 12309848576,
+    note: "Provider 1 note",
+  }
+]
+
+const providerSlice = createSlice({
+  name: 'provider',
+  initialState,
+  reducers: {
+    createProvider (state, action){
+      state.push(action.payload)
+    },
+    getAllProviders (state, action){
+      state.push(action.payload)
+    },
+    deleteProviders (state, action){
+      state.push(action.payload)
+    }
+  }
+})
+
+export default  providerSlice.reducer
+export const { createProvider, getAllProviders, deleteProviders } = providerSlice.actions
+export type { providerType }
